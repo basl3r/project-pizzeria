1e1cd9bab4fc0c6649e2089bd265f7441fa91f02
Adrian Grociak <ajgrociak@gmail.com>
2023-10-19 14:42:19 +0200

    Create class Product and add methods app.initData, app initMenu to render products on main page

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-// eslint-disable-line no-unused-vars
+/* global utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
   'use strict';
@@ -53,19 +53,46 @@
   };
 
   class Product {
-    constructor() {
+    constructor(id, data) {
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+      console.log('new Product: ', thisProduct);
+    }
+
+    renderInMenu() {
       const thisProduct = this;
 
-      console.log('new Product: ', thisProduct);
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct (thisProduct.data);
+      console.log('generatedHTML: ', generatedHTML);
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
   const app = {
     initMenu: function() {
-      const testProduct = new Product();
-      console.log('testProduct', testProduct);
+      console.log('** initMenu started **');
+      const thisApp = this;
+      console.log('thisApp in initMenu: ', thisApp);
+      console.log('thisApp.data: ', thisApp.data);
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
     },
-    
+
+    initData: function () {
+      console.log('** initData started **');
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -73,6 +100,7 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+      thisApp.initData();
       thisApp.initMenu();
     }
   };
