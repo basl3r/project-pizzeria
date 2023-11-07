d5455839625716acf707f90bfea3de41ba732b87
Adrian Grociak <ajgrociak@gmail.com>
2023-11-07 21:19:14 +0100

    Add initPages, activatePage and initBooking methods

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,11 +1,14 @@
 export const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
-    cartProduct: '#template-cart-product', // CODE ADDED
+    cartProduct: '#template-cart-product', 
+    bookingWidget: '#template-booking-widget', //CODE ADDED
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
+    pages: '#pages', //CODE ADDED
+    booking: '.booking-wrapper',
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -25,6 +28,15 @@ export const select = {
       input: 'input.amount', // CODE CHANGED
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
+    },
+    datePicker: {
+      wrapper: '.date-picker',
+      input: `input[name="date"]`,
+    },
+    hourPicker: {
+      wrapper: '.hour-picker',
+      input: 'input[type="range"]',
+      output: '.output',
     },
   },
   // CODE ADDED START
@@ -47,6 +59,14 @@ export const select = {
     edit: '[href="#edit"]',
     remove: '[href="#remove"]',
   },
+  booking: {
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+  },
+  nav: {
+    links: '.main-nav a',
+  },
   // CODE ADDED END
 };
 
@@ -55,10 +75,21 @@ export const classNames = {
     wrapperActive: 'active',
     imageVisible: 'active',
   },
-  // CODE ADDED START
+
   cart: {
     wrapperActive: 'active',
   },
+  // CODE ADDED START
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  }
   // CODE ADDED END
 };
 
@@ -68,7 +99,6 @@ export const settings = {
     defaultMin: 1,
     defaultMax: 9,
   }, // CODE CHANGED
-  // CODE ADDED START
   cart: {
     defaultDeliveryFee: 20,
   },
@@ -76,13 +106,34 @@ export const settings = {
     url: '//localhost:3131',
     products: 'products',
     orders: 'orders',
+    // CODE ADDED START
+    bookings: 'bookings',
+    events: 'events',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
+  },
+  // CODE ADDED START
+  hours: {
+    open: 12,
+    close: 24,
+  },
+  datePicker: {
+    maxDaysInFuture: 14,
+  },
+  booking: {
+    tableIdAttribute: 'data-table',
   },
   // CODE ADDED END
 };
 
 export const templates = {
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-  // CODE ADDED START
+  
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+  
+  // CODE ADDED START
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
   // CODE ADDED END
 };
